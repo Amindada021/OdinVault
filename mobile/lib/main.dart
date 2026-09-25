6c1b56c0e236cebe3222663a75b241214b519fb7
amin.rzzdh <94660683+Amindada021@users.noreply.github.com>
2026-09-25 04:33:01 +0330

    Add OdinVault mobile source: lib/main.dart

---
+++ mobile/lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:odinvault_mobile/odinvault/app.dart';
+import 'package:flutter/material.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const OdinVaultApp());
+}
