a98e03e37635450c24e8ee907475cf8da9a4634a
amin.rzzdh <94660683+Amindada021@users.noreply.github.com>
2026-09-26 08:29:08 +0330

    Disambiguate WPF UserControl from tray interop

--- src/OdinVault.Manager.Wpf/Views/StorageView.xaml.cs
+++ src/OdinVault.Manager.Wpf/Views/StorageView.xaml.cs
@@ -1,1 +1,1 @@
-using System.Windows.Controls;namespace OdinVault.Manager.Wpf.Views;public partial class StorageView:UserControl{public StorageView(){InitializeComponent();}}
+using System.Windows.Controls;namespace OdinVault.Manager.Wpf.Views;public partial class StorageView:System.Windows.Controls.UserControl{public StorageView(){InitializeComponent();}}
