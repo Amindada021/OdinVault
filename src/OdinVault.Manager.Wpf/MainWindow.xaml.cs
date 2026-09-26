1ac6889c3ff27a7123d4916cc05fb1314abfd5b6
amin.rzzdh <94660683+Amindada021@users.noreply.github.com>
2026-09-26 10:07:54 +0330

    Wire WPF about page

--- src/OdinVault.Manager.Wpf/MainWindow.xaml.cs
+++ src/OdinVault.Manager.Wpf/MainWindow.xaml.cs
@@ -17,6 +17,7 @@ public partial class MainWindow : Window
  void Dashboard_Click(object sender,RoutedEventArgs e){SetActiveNav(sender);PageHost.Visibility=Visibility.Collapsed;DashboardPanel.Visibility=Visibility.Visible;}
  async void Databases_Click(object sender,RoutedEventArgs e){SetActiveNav(sender);var db=new DatabasesViewModel(vm.Client);var view=new DatabasesView{DataContext=db};PageHost.Content=view;DashboardPanel.Visibility=Visibility.Collapsed;PageHost.Visibility=Visibility.Visible;await db.LoadAsync();}
  async void Backups_Click(object sender,RoutedEventArgs e){SetActiveNav(sender);var model=new BackupsViewModel(vm.Client);PageHost.Content=new BackupsView{DataContext=model};DashboardPanel.Visibility=Visibility.Collapsed;PageHost.Visibility=Visibility.Visible;await model.LoadAsync();}
+ void About_Click(object s,RoutedEventArgs e){SetActiveNav(s);ShowPage(new AboutView());}
  void Update_Click(object s,RoutedEventArgs e){SetActiveNav(s);ShowPage(new UpdateView());}
  void Mobile_Click(object s,RoutedEventArgs e){SetActiveNav(s);ShowPage(new MobileView{DataContext=new MobileViewModel(vm.Client)});}
  void Settings_Click(object s,RoutedEventArgs e){SetActiveNav(s);ShowPage(new SettingsView(settingsStore,settings,ApplySettings));}
