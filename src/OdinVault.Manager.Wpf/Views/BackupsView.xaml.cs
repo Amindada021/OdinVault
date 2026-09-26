16f62e0031a8591a3e0f0d54b3ec144159423b18
amin.rzzdh <94660683+Amindada021@users.noreply.github.com>
2026-09-26 08:28:50 +0330

    Disambiguate WPF UserControl from tray interop

--- src/OdinVault.Manager.Wpf/Views/BackupsView.xaml.cs
+++ src/OdinVault.Manager.Wpf/Views/BackupsView.xaml.cs
@@ -1,1 +1,1 @@
-using System.Windows.Controls;namespace OdinVault.Manager.Wpf.Views;public partial class BackupsView:UserControl{public BackupsView(){InitializeComponent();}}
+using System.Windows.Controls;namespace OdinVault.Manager.Wpf.Views;public partial class BackupsView:System.Windows.Controls.UserControl{public BackupsView(){InitializeComponent();}}
