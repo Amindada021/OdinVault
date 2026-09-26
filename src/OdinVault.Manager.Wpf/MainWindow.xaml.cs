c2ce3497d15d70a19e949cb995fa031998264223
amin.rzzdh <94660683+Amindada021@users.noreply.github.com>
2026-09-26 08:17:32 +0330

    Wire WPF database navigation

--- src/OdinVault.Manager.Wpf/MainWindow.xaml.cs
+++ src/OdinVault.Manager.Wpf/MainWindow.xaml.cs
@@ -1,9 +1,12 @@
 using System.Windows;
 using OdinVault.Manager.Wpf.ViewModels;
+using OdinVault.Manager.Wpf.Views;
 namespace OdinVault.Manager.Wpf;
 public partial class MainWindow : Window
 {
  readonly MainViewModel vm=new(); bool dark;
  public MainWindow(){InitializeComponent();DataContext=vm;Loaded+=async(_,_)=>await vm.RefreshAsync();Closed+=(_,_)=>vm.Dispose();}
+ void Dashboard_Click(object sender,RoutedEventArgs e){PageHost.Visibility=Visibility.Collapsed;DashboardPanel.Visibility=Visibility.Visible;}
+ async void Databases_Click(object sender,RoutedEventArgs e){var db=new DatabasesViewModel(vm.Client);var view=new DatabasesView{DataContext=db};PageHost.Content=view;DashboardPanel.Visibility=Visibility.Collapsed;PageHost.Visibility=Visibility.Visible;await db.LoadAsync();}
  void Theme_Click(object sender,RoutedEventArgs e){dark=!dark;var merged=Application.Current.Resources.MergedDictionaries;merged[0]=new ResourceDictionary{Source=new Uri(dark?"Themes/Dark.xaml":"Themes/Light.xaml",UriKind.Relative)};}
 }
