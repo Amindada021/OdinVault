047426106ad5e76f66ce038a81be0e47f07f3037
amin.rzzdh <94660683+Amindada021@users.noreply.github.com>
2026-09-26 19:49:03 +0330

    Fix mobile analyzer import warning

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:odinvault_mobile/odinvault/app.dart';
 import 'package:odinvault_mobile/odinvault/monitoring_service.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 Future<void> main() async {
